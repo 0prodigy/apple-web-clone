c7517d324dab246f705a1af91ce96470f8e14b3d
Akash Pathak <pathakvikash9211@gamil.com>
2020-04-01 18:40:58 +0530

    add style to search bar on focus

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let input = document.getElementById("input");
 let nav = document.getElementsByTagName("ul");
 let search = document.getElementsByClassName("search");
+let fa = document.getElementsByClassName("fa-search")
 input.addEventListener("focus", function () {
     nav[0].style.width = "781px";
     search[0].style.width = "170px";
+    fa[0].style.color = "#333"
 });
 
 function focusOut() {
